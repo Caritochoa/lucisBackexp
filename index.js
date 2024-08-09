61bbeb362bfcea8b39a163709cc37ed0ff09cbde
Carolina Ochoa <karittochoa@gmail.com>
2024-08-09 16:09:37 -0500

    cors library implementation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const cors = require('cors');
 
-app.use(cors({
-    origin: '*',  
-  }));
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
 });
