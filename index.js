8f10f3c4836a47a9ed7acb62a150eab986d3b648
Carolina Ochoa <karittochoa@gmail.com>
2024-08-09 15:44:56 -0500

    open cors in a temporary way

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-// Define un endpoint en la ruta raíz que retorna "Hola Mundo"
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
 });
 
-// Inicia el servidor en el puerto especificado
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
