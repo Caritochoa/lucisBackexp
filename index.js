054561507a01c540787494f68c471a9e8f5e6491
Carolina Ochoa <karittochoa@gmail.com>
2024-08-28 20:52:54 -0500

    img endpoint working

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 8000;
-const cors = require('cors');
 
-app.use(cors());
+// Middleware para manejar JSON
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo');
+// Middleware para manejar datos URL-encoded (si estás usando formularios)
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/img/', (req, res) => {
+  console.log('req', req.body.image);
+  const base64Image = req.body.image;
+
+  // Verificar que la imagen base64 esté presente y sea un string
+  if (!base64Image || typeof base64Image !== 'string') {
+    return res.status(400).json({ message: 'Imagen inválida o no proporcionada' });
+  }
+  console.log('imagen recibida:', base64Image)
+
+  // Responder con un mensaje simple
+  res.json({ message: 'Imagen recibida correctamente' });
+ 
 });
 
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-
